a5b6887a673d3dfc9d107172810512fb2f8375c2
nox <psijic213@gmail.com>
2019-01-29 16:25:12 +0900

    Add load_elf_binary and elf_map in apager.c + Modify apager.h

--- apager.c
+++ apager.c
@@ -12,34 +12,40 @@
 int main(int argc, char *argv[])
 {
 	Elf64_Ehdr elf_header;
-	Elf64_Phdr *ph;
+	Elf64_Phdr phdr;
 	int fd, poff;
 
-	if (argc != 2) {
+	if (argc < 2) {
 		fprintf(stderr, "./apager exe_file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	if ((fd = open(argv[1], O_RDONLY)) < 0) {
 		fprintf(stderr, "Fail to open loaded file\n");
-		exit(EXIT_FAILURE);
+		goto out;
 	}
 
 	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) < 0) {
 		fprintf(stderr, "read error %d\n", __LINE__);
-		exit(EXIT_FAILURE);
+		goto out;
 	}
 
 	if (memcmp(&elf_header, ELFMAG, SELFMAG) != 0) {
-		fprintf(stderr, "File Format error\n");
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "File format error\n");
+		goto out;
 	}
 
 	show_elf_header(&elf_header);
 
 	poff = elf_header.e_phoff;
-	lseek(fd, 0, SEEK_SET);
+	lseek(fd, poff, SEEK_SET);
+	read(fd, &phdr, sizeof(Elf64_Phdr));
+	printf("%p\n", mmap(phdr.p_vaddr + 0x1000, phdr.p_filesz, 
+				PROT_READ, MAP_PRIVATE | MAP_FIXED,
+				fd, phdr.p_offset));
 
+out:
+	close(fd);
 	exit (EXIT_SUCCESS);
 }
 
@@ -55,4 +61,58 @@ void show_elf_header(Elf64_Ehdr *ep)
 	printf("e_phnum: %u\n", ep->e_phnum);
 }
 
+int load_elf_binary(int fd, Elf64_Ehdr *ep)
+{
+	Elf64_Phdr phdr;
+	Elf64_Addr elf_entry;
+	int i;
+
+	memset(&phdr, 0, sizeof(Elf64_Phdr));
+	lseek(fd, ep->e_phoff, SEEK_SET);
+	for (i = 0; i < ep->e_phnum; i++) {
+		int elf_prot = 0, elf_flags;
+		Elf64_Addr vaddr;
+
+		if (read(fd, &phdr, sizeof(Elf64_Phdr)) < 0) {
+			fprintf(stderr, "read erorr on phdr\n");
+			return -1;
+		}
+		
+		if (phdr.p_type != PT_LOAD)
+			continue;
+
+		if (phdr.p_flags & PF_R)
+			elf_prot |= PROT_READ;
+		if (phdr.p_flags & PF_W)
+			elf_prot |= PROT_WRITE;
+		if (phdr.p_flags & PF_X)
+			elf_prot |= PROT_EXEC;
+		
+		elf_flags = MAP_PRIVATE | MAP_FIXED | MAP_EXECUTABLE;
+
+		vaddr = phdr.p_vaddr;
+
+		if (elf_map(vaddr, 0, elf_prot, elf_flags, fd, &phdr) < 0) {
+			fprintf(stderr, "elf_map error\n");
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+int elf_map(Elf64_Addr addr, unsigned long total_size, int prot, int type, 
+		int fd, Elf64_Phdr *pp)
+{
+	Elf64_Addr map_addr;
+	unsigned long size = pp->p_filesz + ELF_PAGEOFFSET(pp->p_vaddr);
+	unsigned long off = pp->p_offset - ELF_PAGEOFFSET(pp->p_vaddr);
+	addr = ELF_PAGESTART(addr);
+	size = ELF_PAGEALIGN(size);
+
+	if (!size)
+		return addr;
+	
+	return mmap(addr, size, prot, type, fd, off);
+}
 
--- apager.h
+++ apager.h
@@ -1,11 +1,19 @@
 #ifndef _APAGER_H_
 #define _APAGER_H_
 
-#include <elf.h>
-
 #define	ELFMAG		"\177ELF"
 #define	SELFMAG		4
 
+#define PAGE_SIZE	4096
+#define ELF_MIN_ALIGN	PAGE_SIZE
+
+#define ELF_PAGESTART(_v) ((_v) & ~(unsigned long)(ELF_MIN_ALIGN-1))
+#define ELF_PAGEOFFSET(_v) ((_v) & (ELF_MIN_ALIGN-1))
+#define ELF_PAGEALIGN(_v) (((_v) + ELF_MIN_ALIGN - 1) & ~(ELF_MIN_ALIGN - 1))
+
 void show_elf_header(Elf64_Ehdr *elf_header);
+int load_elf_binary(int fd, Elf64_Ehdr *ep);
+int elf_map(Elf64_Addr addr, unsigned long total_size, int prot, int type, 
+		int fd, Elf64_Phdr *pp);
 
 #endif
